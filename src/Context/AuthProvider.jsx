c046ffcaf339807c50dfa856977b1eeb36253585
Ashraf Azad <ashrafazad86@gmail.com>
2025-01-15 14:19:23 +0600

    created useAxiosSecure Hook

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -10,15 +10,19 @@ import {
   signOut,
 } from "firebase/auth";
 import auth from "../Firebase/firebase.config";
+import useAxiosPublic from "../hooks/useAxiosPublic";
+
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const axiosPublic = useAxiosPublic();
   const googleProvider = new GoogleAuthProvider();
 
   const createUserWithEmailPass = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
+
   const signinWithGoogle = () => {
     return signInWithPopup(auth, googleProvider);
   };
@@ -32,15 +36,31 @@ const AuthProvider = ({ children }) => {
   };
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (acc) => {
-      setUser(acc);
-      setLoading(false);
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      if (currentUser) {
+        const userInfo = { email: currentUser.email };
+        axiosPublic
+          .post("/jwt", userInfo)
+          .then((res) => {
+            if (res.data.token) {
+              localStorage.setItem("access-token", res.data.token);
+              setLoading(false);
+            }
+          })
+          .catch((error) => {
+            console.error("Error getting JWT:", error);
+            setLoading(false)
+          });
+      } else {
+        localStorage.removeItem("access-token");
+        setLoading(false);
+      }
     });
-
     return () => {
       return unsubscribe();
     };
-  }, []);
+  }, [axiosPublic]);
 
   const contextValues = {
     user,
@@ -57,7 +77,9 @@ const AuthProvider = ({ children }) => {
     </AuthContext.Provider>
   );
 };
+
 AuthProvider.propTypes = {
   children: PropTypes.node.isRequired,
 };
+
 export default AuthProvider;
